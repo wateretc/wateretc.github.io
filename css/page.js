ea9ef900bee95c86fa122ceb400373cb42d2f9d8
Tom Pro <36899577+wateretc@users.noreply.github.com>
2023-05-11 13:35:14 -0400

    Delete css directory

--- css/page.js
+++
@@ -1,9 +0,0 @@
-// Fade in animation
-const pageElements = document.getElementsByClassName('page');
-
-if (pageElements.length > 0) {
-    const page = pageElements[0];
-    setTimeout(function() {
-        page.classList.remove('hidden');
-    }, 800);
-}
